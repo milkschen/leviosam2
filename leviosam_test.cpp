84daecf25a2c870fb9481b440977ce7558b8ff93
Taher Mun <tmun1@jhu.edu>
2021-03-06 23:46:44 -0500

    added some more tests and simple test data

--- leviosam_test.cpp
+++ leviosam_test.cpp
@@ -1,5 +1,15 @@
+#include <iostream>
+#include <unistd.h>
+#include <string>
 #include "leviosam.hpp"
 #include "gtest/gtest.h"
+
+TEST(Ctest, WorkingDir) {
+    char buf[1024];
+    std::string cwd(getcwd(buf, 1024));
+    std::string x("/storage/tmun/levioSAM_exps/levioSAM/testdata");
+    EXPECT_EQ(buf, x);
+}
 
 //
 // bit_vector tests
@@ -160,18 +170,53 @@ TEST(Lift, SimpleDelLift) {
     EXPECT_EQ(lift.s2_to_s1(6), 7);
 }
 
-TEST(Lift, SimpleDelLift) {
-    sdsl::bit_vector ibv;
-    sdsl::bit_vector dbv;
-    sdsl::bit_vector sbv; // ins, del, snp
-    ibv.resize(20);
-    dbv.resize(20);
-    sbv.resize(20);
-    dbv[5] = 1;
-    //     01234567
-    // I : 00000000000000000000
-    // D : 00000100000000000000
-    //     01234 56
-    lift::Lift lift(ibv, dbv, sbv);
-    EXPECT_EQ(lift.s2_to_s1(6), 7);
+
+TEST(LiftMap, SimpleBamLift) {
+    std::vector<std::string> chroms_not_found;
+    std::mutex mutex_vec;
+    vcfFile* vcf_fp = bcf_open("simple_example.vcf", "r");
+    bcf_hdr_t* vcf_hdr = bcf_hdr_read(vcf_fp);
+    lift::LiftMap lmap(vcf_fp, vcf_hdr, "", "0");
+    samFile* sam_fp = sam_open("simple_example.sam", "r");
+    bam_hdr_t* sam_hdr = sam_hdr_read(sam_fp);
+    bam1_t* aln = bam_init1();
+    int err;
+    size_t x;
+    err = sam_read1(sam_fp, sam_hdr, aln);
+    x = lmap.s2_to_s1(sam_hdr->target_name[aln->core.tid], aln->core.pos, &chroms_not_found, &mutex_vec);
+    EXPECT_EQ(x, 5313299);
+    err = sam_read1(sam_fp, sam_hdr, aln);
+    EXPECT_EQ(err, 0);
+    x = lmap.s2_to_s1(sam_hdr->target_name[aln->core.tid], aln->core.pos, &chroms_not_found, &mutex_vec);
+    EXPECT_EQ(x, 5315929);
+}
+
+TEST(LiftMap, SimpleBamCigarLift) {
+    std::vector<std::string> chroms_not_found;
+    std::mutex mutex_vec;
+    vcfFile* vcf_fp = bcf_open("simple_example.vcf", "r");
+    bcf_hdr_t* vcf_hdr = bcf_hdr_read(vcf_fp);
+    lift::LiftMap lmap(vcf_fp, vcf_hdr, "", "0");
+    samFile* sam_fp = sam_open("simple_example.sam", "r");
+    bam_hdr_t* sam_hdr = sam_hdr_read(sam_fp);
+    bam1_t* aln = bam_init1();
+    int err;
+    size_t x;
+    uint32_t* test_cigar;
+    err = sam_read1(sam_fp, sam_hdr, aln);
+    lmap.cigar_s2_to_s1(sam_hdr->target_name[aln->core.tid], aln);
+    test_cigar = bam_get_cigar(aln);
+    EXPECT_EQ(aln->core.n_cigar, 3);
+    EXPECT_EQ(test_cigar[0], bam_cigar_gen( 11, BAM_CMATCH));
+    EXPECT_EQ(test_cigar[1], bam_cigar_gen( 2, BAM_CDEL));
+    EXPECT_EQ(test_cigar[2], bam_cigar_gen( 9, BAM_CMATCH));
+
+    err = sam_read1(sam_fp, sam_hdr, aln);
+    EXPECT_EQ(err, 0);
+    lmap.cigar_s2_to_s1(sam_hdr->target_name[aln->core.tid], aln);
+    test_cigar = bam_get_cigar(aln);
+    EXPECT_EQ(aln->core.n_cigar, 3);
+    EXPECT_EQ(test_cigar[0], bam_cigar_gen( 15, BAM_CMATCH));
+    EXPECT_EQ(test_cigar[1], bam_cigar_gen( 1, BAM_CDEL));
+    EXPECT_EQ(test_cigar[2], bam_cigar_gen( 5, BAM_CMATCH));
 }
