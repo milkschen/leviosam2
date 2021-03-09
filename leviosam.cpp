1c1b72e50e819c61b3ed081df774d6d112bfb29f
Taher Mun <tmun1@jhu.edu>
2021-03-09 16:01:51 -0500

    set up fillmd code to optionally add MD/NM tags. Removes MD/NM from aln otherwise

--- leviosam.cpp
+++ leviosam.cpp
@@ -1,13 +1,23 @@
-#include "leviosam.hpp"
 #include <vector>
 #include <tuple>
-#include <unordered_map>
-#include <getopt.h>
+#include <map>
 #include <cstdio>
 #include <thread>
+#include <getopt.h>
 #include <htslib/vcf.h>
 #include <htslib/sam.h>
 #include <htslib/kstring.h>
+#include <htslib/kseq.h>
+#include <zlib.h>
+#include <stdio.h>
+#include "leviosam.hpp"
+
+KSEQ_INIT(gzFile, gzread)
+;;
+
+extern "C" {
+    int bam_fillmd1(bam1_t *b, const char *ref, int flag, int quiet_mode);
+}
 
 struct lift_opts {
     std::string vcf_fname = "";
@@ -23,6 +33,8 @@ struct lift_opts {
     int verbose = 0;
     NameMap name_map;
     LengthMap length_map;
+    int md_flag = 0;
+    std::string ref_name = "";
 };
 
 NameMap parse_name_map(const char* fname) {
@@ -72,7 +84,6 @@ void serialize_run(lift_opts args) {
     fprintf(stderr, "levioSAM file saved to %s\n", (args.outpre + ".lft").data());
 }
 
-
 void read_and_lift(
     std::mutex* mutex_fread,
     std::mutex* mutex_fwrite,
@@ -82,8 +93,12 @@ void read_and_lift(
     bam_hdr_t* hdr,
     int chunk_size,
     lift::LiftMap* l,
-    std::vector<std::string>* chroms_not_found
+    std::vector<std::string>* chroms_not_found,
+    std::map<std::string, std::string>* ref_dict,
+    int md_flag
 ){
+    std::string ref_name;
+    std::string ref;
     std::vector<bam1_t*> aln_vec;
     for (int i = 0; i < chunk_size; i++){
         bam1_t* aln = bam_init1();
@@ -141,6 +156,23 @@ void read_and_lift(
                     aln->core.isize = isize;
                 }
             }
+            if (md_flag) {
+                // change ref if needed
+                if (s1_name != ref_name) {
+                    ref = (*ref_dict)[s1_name];
+                }
+                bam_fillmd1(aln, ref.data(), md_flag, 1);
+            }
+            else { // strip MD and NM tags if md_flag not set bc the liftover invalidates them
+                uint8_t* ptr = NULL;
+                if ((ptr = bam_aux_get(aln, "MD")) != NULL) {
+                    bam_aux_del(aln, ptr);
+                }
+                if ((ptr = bam_aux_get(aln, "NM")) != NULL) {
+                    bam_aux_del(aln, bam_aux_get(aln, "NM"));
+                }
+            }
+            ref_name = s1_name;
         }
         {
             // write to file, thread corruption protected by lock_guard
@@ -159,6 +191,19 @@ void read_and_lift(
         bam_destroy1(aln_vec[i]);
     }
     aln_vec.clear();
+}
+
+std::map<std::string, std::string> load_fasta(std::string ref_name) {
+    std::map<std::string, std::string> fmap;
+    gzFile fp = gzopen(ref_name.data(), "r");
+    kseq_t *seq;
+    seq = kseq_init(fp);
+    while (kseq_read(seq) >= 0) {
+        fmap[std::string(seq->name.s)] = std::string(seq->seq.s);
+    }
+    kseq_destroy(seq);
+    gzclose(fp);
+    return fmap;
 }
 
 void lift_run(lift_opts args) {
@@ -210,7 +255,18 @@ void lift_run(lift_opts args) {
         }
     }
     auto write_hdr = sam_hdr_write(out_sam_fp, hdr);
-    
+
+
+    std::map<std::string, std::string> ref_dict;
+    if (args.md_flag) {
+        // load
+        if (args.ref_name == "") {
+            std::cerr << "error: -m/--md -f <fasta> to be provided as well\n";
+            exit(1);
+        }
+        ref_dict = load_fasta(args.ref_name);
+    }
+
     // Store chromosomes found in SAM but not in the VCF.
     // We use a vector to avoid printing out the same warning msg multiple times.
     std::vector<std::string> chroms_not_found;
@@ -230,7 +286,9 @@ void lift_run(lift_opts args) {
                 hdr,
                 chunk_size,
                 &l,
-                &chroms_not_found
+                &chroms_not_found,
+                &ref_dict,
+                args.md_flag
             )
         );
     }
@@ -243,9 +301,9 @@ void lift_run(lift_opts args) {
 }
 
 
-lift::LiftMap lift_from_vcf(std::string fname, 
-                            std::string sample, 
-                            std::string haplotype, 
+lift::LiftMap lift_from_vcf(std::string fname,
+                            std::string sample,
+                            std::string haplotype,
                             NameMap names, LengthMap lengths) {
     if (fname == "" && sample == "") {
         fprintf(stderr, "vcf file name and sample name are required!! \n");
@@ -294,8 +352,10 @@ void print_lift_help_msg(){
     fprintf(stderr, "         -T INT    Chunk size for each thread. [256] \n");
     fprintf(stderr, "                   Each thread queries <-T> reads, lifts, and writes.\n");
     fprintf(stderr, "                   Setting a higher <-T> uses slightly more memory but might benefit thread scaling.\n");
+    fprintf(stderr, "         -m        add MD and NM to output alignment records (requires -f option)\n");
+    fprintf(stderr, "         -f string Fasta reference that corresponds to input SAM/BAM (for use w/ -m option)\n");
     fprintf(stderr, "         The options for serialize can also be used here, if -v is set.\n");
-    print_serialize_help_msg();
+    fprintf(stderr, "\n");
 }
 
 void print_main_help_msg(){
@@ -323,10 +383,12 @@ int main(int argc, char** argv) {
         {"haplotype", required_argument, 0, 'g'},
         {"threads", required_argument, 0, 't'},
         {"chunk_size", required_argument, 0, 'T'},
+        {"md", required_argument, 0, 'm'},
+        {"nm", required_argument, 0, 'x'},
         {"verbose", no_argument, &args.verbose, 1},
     };
     int long_index = 0;
-    while((c = getopt_long(argc, argv, "hv:s:p:l:a:O:g:n:k:t:T:", long_options, &long_index)) != -1) {
+    while((c = getopt_long(argc, argv, "hv:s:p:l:a:O:g:n:k:t:T:mf:", long_options, &long_index)) != -1) {
         switch (c) {
             case 'v':
                 args.vcf_fname = optarg;
@@ -361,7 +423,15 @@ int main(int argc, char** argv) {
             case 'T':
                 args.chunk_size = atoi(optarg);
                 break;
+            case 'm':
+                args.md_flag |= 8;
+                args.md_flag |= 16;
+                break;
+            case 'f':
+                args.ref_name = std::string(optarg);
+                break;
             case 'h':
+                print_serialize_help_msg();
                 print_lift_help_msg();
                 exit(1);
             default:
