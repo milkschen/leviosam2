594134f49117eca66f38f3031e52955f9f812d76
milkschen <s93rm6@gmail.com>
2021-09-09 18:49:26 -0400

    measure time; clean unused lines

--- selective_liftover/leviosam_refflow.sh
+++ selective_liftover/leviosam_refflow.sh
@@ -8,47 +8,34 @@ BT2_IDX="/net/langmead-bigmem-ib.bluecrab.cluster/storage2/naechyun/fasta/grch38
 BT2_RG="--rg-id chm13to38 --rg SM:HG002 --rg LB:chm13 --rg PL:ILLUMINA --rg DS:novaseq --rg PU:novaseq"
 LEVIOSAM="$NC2/levioSAM/leviosam"
 CLFT="$NC2/fasta/t2t-chm13-v1.0.hg38.clft"
+TIME="/home-1/cnaechy1@jhu.edu/bin/time-1.9"
 
 # Check inputs
 if [ ! -f ${PFX}.bam ]; then
-    ${LEVIOSAM} lift -C ${CLFT} -a ${INPUT} -t ${TH} -p ${PFX} -O bam -S mapq
+    ${TIME} -v -o lift.time_log ${LEVIOSAM} lift -C ${CLFT} -a ${INPUT} -t ${TH} -p ${PFX} -O bam -S mapq
 fi
-
-# if [ ! -f ${PFX}-deferred.bam ]; then
-#     echo "Missing input: ${PFX}-deferred.bam"
-#     exit
-# fi
-# if [ ! -f ${PFX}.bam ]; then
-#     echo "Missing input: ${PFX}.bam"
-#     exit
-# fi
 
 if [ ! -f ${PFX}-deferred-collate.bam ]; then
-    samtools collate -@ ${TH} -fo ${PFX}-deferred-collate.bam ${PFX}-deferred.bam
+    ${TIME} -v -o collate.time_log samtools collate -@ ${TH} -fo ${PFX}-deferred-collate.bam ${PFX}-deferred.bam
 fi
 FQSIZE=$(stat -c%s "${PFX}-deferred-R1.fq")
-# if [ ! -f ${PFX}-deferred-R1.fq ]; then
 if (( ${FQSIZE} > 0 )); then
     echo "Skip converting FASTQ"
 else
-    samtools fastq -1 ${PFX}-deferred-R1.fq -2 ${PFX}-deferred-R2.fq -s ${PFX}-deferred-S.fq ${PFX}-deferred-collate.bam
+    ${TIME} -v -o to_fastq.time_log samtools fastq -1 ${PFX}-deferred-R1.fq -2 ${PFX}-deferred-R2.fq -s ${PFX}-deferred-S.fq ${PFX}-deferred-collate.bam
 fi
 
 if [ ! -f ${PFX}-deferred-re_aligned-paired.bam ]; then
-    bowtie2 ${BT2_RG} -p ${TH} -x ${BT2_IDX} -1 ${PFX}-deferred-R1.fq -2 ${PFX}-deferred-R2.fq | samtools sort -@ ${STH} -o ${PFX}-deferred-re_aligned-paired.bam
+    ${TIME} -v -o aln_paired.time_log bowtie2 ${BT2_RG} -p ${TH} -x ${BT2_IDX} -1 ${PFX}-deferred-R1.fq -2 ${PFX}-deferred-R2.fq | samtools sort -@ ${STH} -o ${PFX}-deferred-re_aligned-paired.bam
 fi
 if [ ! -f ${PFX}-deferred-re_aligned-unpaired.bam ]; then
-    bowtie2 ${BT2_RG} -p ${TH} -x ${BT2_IDX} -U ${PFX}-deferred-S.fq | samtools sort -@ ${STH} -o ${PFX}-deferred-re_aligned-unpaired.bam
+    ${TIME} -v -o aln_unpaired.time_log bowtie2 ${BT2_RG} -p ${TH} -x ${BT2_IDX} -U ${PFX}-deferred-S.fq | samtools sort -@ ${STH} -o ${PFX}-deferred-re_aligned-unpaired.bam
 fi
 
-
-# if [ ! -f  ${PFX}-committed.bam ]; then
-#     samtools addreplacerg -r ID:chm13to38 -r LB:chm13 -r PL:ILLUMINA -r SM:HG002 -r PU:novaseq ${PFX}.bam | samtools sort -@ ${STH} -o ${PFX}-committed.bam
-#     # DS:novaseq
-#     # samtools addreplacerg -r "@RG	ID:HG002	LB:grch38	PL:illumina	SM:grch38	PU:novaseq" ${PFX}.bam | samtools sort -@ ${STH} -o ${PFX}-committed.bam
-# fi
+if [ ! -f ${PFX}-merged.bam ]; then
+    ${TIME} -v -o merge.time_log samtools merge ${PFX}-merged.bam ${PFX}.bam ${PFX}-deferred-re_aligned-paired.bam ${PFX}-deferred-re_aligned-unpaired.bam
+fi
 
 if [ ! -f ${PFX}-final.bam ]; then
-    samtools merge ${PFX}-final.bam ${PFX}.bam ${PFX}-deferred-re_aligned-paired.bam ${PFX}-deferred-re_aligned-unpaired.bam
-    # samtools merge ${PFX}-final.bam ${PFX}-committed.bam ${PFX}-deferred-re_aligned-paired.bam ${PFX}-deferred-re_aligned-unpaired.bam
+    ${TIME} -v -o sort_all.time_log samtools sort -@ ${TH} -o ${PFX}-final.bam ${PFX}-merged.bam
 fi
