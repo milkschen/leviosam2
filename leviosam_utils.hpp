02f924aeb4b38046b4d3bb2c8f1f02583ddf529a
Andrea Guarracino <62253982+AndreaGuarracino@users.noreply.github.com>
2021-10-14 17:38:32 +0200

    added missing #include <cstring>

--- leviosam_utils.hpp
+++ leviosam_utils.hpp
@@ -7,6 +7,7 @@
 #include <htslib/sam.h>
 #include <unordered_map>
 #include <vector>
+#include <cstring>
 
 const int8_t seq_comp_table[16] = { 0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15 };
 
