0331d89b37789d84b4b065368a7997e564508a4c
milkschen <s93rm6@gmail.com>
2022-03-03 21:48:12 -0500

    add a sorting step

--- scripts/vcf_to_bed.sh
+++ scripts/vcf_to_bed.sh
@@ -1,3 +1,3 @@
 # From https://www.biostars.org/p/106249/
 # Usage sh vcf_to_bed.sh intput.vcf > output.bed
-bcftools norm -m- $1 | grep -v '^#' | awk -v OFS='\t' '{if(length($4) > length($5)) print $1,$2,$2+length($4)-1; else print $1,$2-1,$2+length($5)-1}' | bedtools merge -i -
+bcftools norm -m- $1 | grep -v '^#' | awk -v OFS='\t' '{if(length($4) > length($5)) print $1,$2,$2+length($4)-1; else print $1,$2-1,$2+length($5)-1}' | bedtools sort -i - | bedtools merge -i -
