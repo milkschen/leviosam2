04199844fc8109f004bfa229a2187d3466cb7567
milkschen <s93rm6@gmail.com>
2022-03-03 21:49:14 -0500

    changed gap setting

--- selective_liftover/leviosam_longreads.sh
+++ selective_liftover/leviosam_longreads.sh
@@ -14,26 +14,30 @@ TIME=time # GNU time
 MEASURE_TIME=1 # Set to a >0 value to measure time for each step
 DEFER_DEST_BED=""
 COMMIT_SOURCE_BED=""
-ALLOWED_GAPS=10
+ALLOWED_GAPS=1000
+HDIST="-S hdist:100"
 ALN=minimap2
+REALN_CONFIG=""
 
 
-while getopts a:C:D:g:F:i:L:M:o:r:R:t:T: flag
+while getopts a:C:D:g:f:H:i:L:M:o:r:R:t:T:x: flag
 do
     case "${flag}" in
         a) ALN=${OPTARG};;
         C) CLFT=${OPTARG};;
         D) DEFER_DEST_BED=" -D ${OPTARG}";;
-        R) COMMIT_SOURCE_BED=" -r ${OPTARG}";;
-        F) REF=${OPTARG};;
+        f) REF=${OPTARG};;
+        H) HDIST=" -S hdist:${OPTARG}";;
         g) ALLOWED_GAPS=${OPTARG};;
         i) INPUT=${OPTARG};;
         L) LEVIOSAM=${OPTARG};;
         M) MEASURE_TIME=${OPTARG};;
         o) PFX=${OPTARG};;
         r) ALN_RG=${OPTARG};;
+        R) COMMIT_SOURCE_BED=" -r ${OPTARG}";;
         t) THR=${OPTARG};;
         T) TIME=${OPTARG};;
+        x) REALN_CONFIG=" -x ${OPTARG}";;
     esac
 done
 
@@ -45,6 +49,7 @@ echo "Aligner read group: ${ALN_RG}";
 echo "LevioSAM software: ${LEVIOSAM}";
 echo "LevioSAM index: ${CLFT}";
 echo "Allowed gaps: ${ALLOWED_GAPS}";
+echo "Re-alignment config: ${REALN_CONFIG}";
 echo "BED where reads get deferred: ${DEFER_DEST_BED}";
 echo "BED where reads get discarded: ${COMMIT_SOURCE_BED}";
 echo "Num. threads: ${THR}";
@@ -62,7 +67,9 @@ fi
 # Lifting over using leviosam
 if [ ! -s ${PFX}-committed.bam ]; then
     ${TT} ${LEVIOSAM} lift -C ${CLFT} -a ${INPUT} -t ${THR} -p ${PFX} -O bam \
-    -S lifted -G ${ALLOWED_GAPS} \
+    -S lifted ${HDIST} -G ${ALLOWED_GAPS} \
+    ${REALN_CONFIG} \
+    -m -f ${REF} \
     ${DEFER_DEST_BED} ${COMMIT_SOURCE_BED}
 fi
 
@@ -74,7 +81,7 @@ fi
 
 # Re-align deferred reads
 if [ ! -s ${PFX}-realigned.bam ]; then
-    ${TT} ${ALN} -ax map-hifi -t ${THR} ${REF} ${PFX}-deferred.fq.gz | \
+    ${TT} ${ALN} -ax map-hifi --MD -t ${THR} -R ${ALN_RG} ${REF} ${PFX}-deferred.fq.gz | \
     ${TT} samtools view -hbo ${PFX}-realigned.bam
 fi
 
