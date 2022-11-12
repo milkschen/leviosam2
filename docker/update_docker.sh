b76e918f79bdd479f49eae110f582ce1cd5b8e4e
milkschen <s93rm6@gmail.com>
2022-11-11 23:18:59 -0800

    update

--- docker/update_docker.sh
+++ docker/update_docker.sh
@@ -15,12 +15,12 @@ docker build --tag leviosam2_env .
 
 # build the production image
 cd ../production
-docker build --tag leviosam2 .
+docker build --tag leviosam2:${VER} --tag leviosam2:latest .
 
 # push using the ${VER} tag
-docker tag leviosam2 naechyun/leviosam2:${VER}
+# docker tag leviosam2 naechyun/leviosam2:${VER}
 docker push naechyun/leviosam2:${VER}
 
 # push using the tag "latest"
-docker tag leviosam2 naechyun/leviosam2:latest
+# docker tag leviosam2 naechyun/leviosam2:latest
 docker push naechyun/leviosam2:latest
